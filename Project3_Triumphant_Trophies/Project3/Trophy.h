9cba954a5eec174acfd52da0892f9bb60338c221
Cris Esparza <cesparza@uccs.edu>
2022-02-13 23:58:06 -0700

    barely started

--- Project3_Triumphant_Trophies/Project3/Trophy.h
+++ Project3_Triumphant_Trophies/Project3/Trophy.h
@@ -1,9 +1,13 @@
 // TODO: Guard the header
-
+#ifndef TROPHY_H
+#define TROPHY_H
 // TODO: include the libraries this class will need
-
-enum Color;		// TODO: create the color values
+#include <iostream>
+#include <string>
+using namespace std;
+enum Color {GOLD, SILVER, BRONZE};		// TODO: create the color values
 
 class Trophy;	// TODO: complete the Trophy class
 
 // TODO: end the guard
+#endif
