b62b81326d3202b2250903ce711eec96b1e8f05f
Cris Esparza <cesparza@uccs.edu>
2022-02-13 23:42:48 -0700

    added project 3

---
+++ Project3_Triumphant_Trophies/Project3/Trophy.h
@@ -0,0 +1,9 @@
+// TODO: Guard the header
+
+// TODO: include the libraries this class will need
+
+enum Color;		// TODO: create the color values
+
+class Trophy;	// TODO: complete the Trophy class
+
+// TODO: end the guard
