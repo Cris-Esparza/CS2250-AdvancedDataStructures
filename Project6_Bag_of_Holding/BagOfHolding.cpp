b3c21dbac4d3519a1a2ad6cb5f795567f8077efb
Cris E <cesparza@uccs.edu>
2022-03-10 00:24:48 -0700

    working on operators

--- Project6_Bag_of_Holding/BagOfHolding.cpp
+++ Project6_Bag_of_Holding/BagOfHolding.cpp
@@ -15,28 +15,24 @@ BagOfHolding::BagOfHolding(const BagOfHolding& list)
 	if (list.m_head != nullptr)
 	{
 		//copy head's data over
-		m_head = new BagOfHoldingItem(list.m_head->getItem(),nullptr);
+		m_head = new BagOfHoldingItem(nullptr);
 
 		//copy over rest of list
 		BagOfHoldingItem* curr = m_head;
-
-		itemCurr = itemCurr->getNext();
 		
 		while (itemCurr != nullptr)
 		{
 			//create new node and copy itemCurr data over
-			BagOfHoldingItem* temp = new BagOfHoldingItem(itemCurr->getItem(), nullptr);
-			curr->setNext(temp);
+			BagOfHoldingItem* temp = new BagOfHoldingItem(nullptr);
+
 			curr = temp;
-			//move itemCurr forward
-			itemCurr = itemCurr->getNext();
 		}
 	}
 }
 
 BagOfHolding::~BagOfHolding()
 {
-	
+	delete m_head;
 }
 
 // Mutators
@@ -52,9 +48,6 @@ void BagOfHolding::addBottom(string item)
 
 string BagOfHolding::removeTop()
 {
-	BagOfHoldingItem* curr = m_head;
-	m_head = m_head->getNext();
-	delete curr;
 	return " ";
 }
 
@@ -65,13 +58,13 @@ string BagOfHolding::removeBottom()
 
 bool BagOfHolding::removeItem(string item)
 {
-	return false;
+	return true;
 }
 
 // Accessors
 bool BagOfHolding::search(string item) const
 {
-	return false;
+	return true;
 }
 
 int BagOfHolding::size() const
@@ -82,35 +75,11 @@ int BagOfHolding::size() const
 // Operators
 BagOfHolding& BagOfHolding::operator=(const BagOfHolding& list)
 {
-	while (m_head != nullptr)
-	{
-		removeTop();
-	}
-
-	BagOfHoldingItem* itemCurr = list.m_head;
-	if (list.m_head != nullptr)
-	{
-		m_head = new BagOfHoldingItem(list.m_head->getItem(),nullptr);
-		BagOfHoldingItem* curr = m_head;
-		itemCurr = itemCurr->getNext();
-		while (itemCurr != nullptr)
-		{
-			BagOfHoldingItem* temp = new BagOfHoldingItem(itemCurr->getItem(),nullptr);
-			curr->setNext(temp);
-			curr = temp;
-			itemCurr = itemCurr->getNext();
-		}
-	}
 	return *this;
 }
 
 ostream& operator<<(ostream& sout, const BagOfHolding& list)
 {
-	BagOfHoldingItem* curr = list.m_head;
-	while (curr != nullptr)
-	{
-		sout << curr->getItem() << endl;
-		curr = curr->getNext();
-	}
+	
 	return sout;
 }
